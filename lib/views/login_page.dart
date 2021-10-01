281f45bcade012659cddc593e02bf8c3b2ce4064
by-German <german280599@gmail.com>
2021-09-30 23:21:53 -0500

    feat: login page implemented

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,10 +1,111 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 60, bottom: 60),
+              child: Image.asset(
+                "assets/images/logo.png",
+                width: 200,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 300,
+                decoration: BoxDecoration(
+                  color: Colors.black87,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)
+                  )
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40, bottom: 40),
+                      child: Text("Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    Form(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 20),
+                        child: Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                labelText: "Email",
+                                border: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white)
+                                ),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                hintText: "name@email.com",
+                                suffixIcon: Icon(Icons.email),
+                                fillColor: Colors.white,
+                                filled: true
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            SizedBox(height: 10),
+                            TextField(
+                              decoration: InputDecoration(
+                                  labelText: "Password",
+                                  border: UnderlineInputBorder(
+                                      borderSide: BorderSide(color: Colors.white)
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white),
+                                  ),
+                                  fillColor: Colors.white,
+                                  filled: true,
+                                  suffixIcon: Icon(Icons.password_sharp),
+                            ),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                            SizedBox(height: 20),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                                fixedSize: MaterialStateProperty.all<Size>(Size.fromWidth(500)),
+                              ),
+                              child: Text("Login"),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                                fixedSize: MaterialStateProperty.all<Size>(Size.fromWidth(500)),
+                                foregroundColor: MaterialStateProperty.all(Colors.black),
+                              ),
+                              child: Text("Register"),
+                            )
+                          ],
+                        ),
+                      )
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
-  
+
 }
