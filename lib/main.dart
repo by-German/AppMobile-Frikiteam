b4e6d0f05a2d7994494530d06559afab9cb7d546
Josmarvs <siriusjomsar123@gmail.com>
2021-10-01 02:45:32 -0500

    feat:implemented list event page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frikiteam/views/listevent_page.dart';
 import 'package:frikiteam/views/register_page.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RegisterPage()
+      home: ListEventPage()
     );
   }
 }
---
+++ lib/views/listevent_page.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class ListEventPage extends StatelessWidget{
+
+ @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {},icon: Icon(Icons.person), iconSize: 50,),
+        actions: [
+          Image.asset("assets/images/logo.png",height: 10,)
+        ],
+        backgroundColor: Colors.black,
+      ),
+
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: Colors.white,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(20),
+                      border: OutlineInputBorder(),
+                      hintText: "Search: ",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 16,)
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              _crearCard1(),
+              _crearCard2(),
+              _crearCard3(),
+              _crearCard4()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard1(){
+    return Card(
+      elevation: 5,
+
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget> [
+            Image.asset("assets/images/otaku.png", height: 150, width: 400,),
+            Text('Otaku', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+            SizedBox(height: 15,),
+            Text('Evento relacionado al mundo del manga, se presentaron productos exclusivos.'),
+            SizedBox(height: 15,),
+            Text('fecha: 05/09/2020',)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearCard2(){
+    return Card(
+      elevation: 5,
+      color: Colors.black,
+
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget> [
+            Image.asset("assets/images/friki.png", height: 150, width: 400,),
+            Text('Friki', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),),
+            SizedBox(height: 15,),
+            Text('Evento relacionado al mundo del manga, se presentaron productos exclusivos.', style: TextStyle(color: Colors.white),),
+            SizedBox(height: 15,),
+            Text('fecha: 05/09/2020',  style: TextStyle(color: Colors.white),)
+          ],
+        ),
+      ),
+    );
+  }
+
+ Widget _crearCard3() {
+   return Card(
+     elevation: 5,
+
+     child: Container(
+       padding: EdgeInsets.all(20),
+       child: Column(
+         children: <Widget>[
+           Image.asset("assets/images/otaku.png", height: 150, width: 400,),
+           Text('Otaku', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+           SizedBox(height: 15,),
+           Text(
+               'Evento relacionado al mundo del manga, se presentaron productos exclusivos.'),
+           SizedBox(height: 15,),
+           Text('fecha: 05/09/2020',)
+         ],
+       ),
+     ),
+   );
+ }
+
+  Widget _crearCard4(){
+    return Card(
+      elevation: 5,
+      color: Colors.black,
+
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget> [
+            Image.asset("assets/images/friki.png", height: 150, width: 400,),
+            Text('Friki', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),),
+            SizedBox(height: 15,),
+            Text('Evento relacionado al mundo del manga, se presentaron productos exclusivos.', style: TextStyle(fontSize: 15,color: Colors.white),),
+            SizedBox(height: 15,),
+            Text('fecha: 05/09/2020',  style: TextStyle(fontSize: 15,color: Colors.white),)
+          ],
+        ),
+      ),
+    );
+  }
+
+}
