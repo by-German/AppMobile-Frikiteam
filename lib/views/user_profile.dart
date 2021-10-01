dadbb8747eddc047adf1ab911b10295afd8372b8
Sebastian <sebastian.rv.2001@gmail.com>
2021-10-01 04:09:48 -0500

    feat: add appbar to user porfile

--- lib/views/user_profile.dart
+++ lib/views/user_profile.dart
@@ -14,6 +14,15 @@ class _UserProfileState extends State<UserProfile>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => LoginPage()));
+        },icon: Icon(Icons.person), iconSize: 50,),
+        actions: [
+          Image.asset("assets/images/logo.png",height: 10,)
+        ],
+        backgroundColor: Colors.black,
+      ),
       body: Stack(
         children: <Widget>[
 
