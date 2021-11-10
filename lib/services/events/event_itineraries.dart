a48d3cdc8bd51ff8ddf6b63cd819a14cfce1548b
by-German <german280599@gmail.com>
2021-11-09 23:35:27 -0500

    feat: add itineraries service

---
+++ lib/services/events/event_itineraries.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:frikiteam/models/events/itinerary.dart';
+import 'package:frikiteam/services/common/http.common.dart';
+import 'package:http/http.dart' as http;
+
+class EventItinerariesService {
+  Future<List<Itinerary>> getAllEventItineraries(int eventId) async {
+    var response = await http.get(Uri.parse('$basePath/events/$eventId/itineraries'));
+    var itineraries = jsonDecode(response.body) as List;
+
+    return itineraries.map((e) => Itinerary.fromJson(e)).toList();
+  }
+
+  Future<void> postEventItineraries(int eventId, List<Itinerary> items) async {
+    for (var item in items) {
+      await http.post(
+          Uri.parse('$basePath/events/$eventId/itineraries'),
+          body: jsonEncode(item),
+          headers: {'Content-type': 'application/json'}
+        );
+    }
+  }
+
+
+}
