fd32ccb99504874742528a2446fcc27c28c691d0
by-German <german280599@gmail.com>
2021-11-25 08:34:26 -0500

    feat: terminos y condiciones en la vista register

--- lib/views/users/register_page.dart
+++ lib/views/users/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frikiteam/services/users/user_auth_service.dart';
 import 'package:frikiteam/views/users/login_page.dart';
 
 class RegisterPage extends StatefulWidget{
@@ -8,173 +9,180 @@ class RegisterPage extends StatefulWidget{
 }
 
 class _RegisterPageState extends State<RegisterPage>{
-  bool _loading = false;
+  UserAuthService authService = UserAuthService();
+  bool processing = false;
+  String message = '';
+
+  String firstName = '';
+  String lastName = '';
+  String email = '';
+  String password = '';
+  bool isOrganizer = false;
+  bool accept = false;
+
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(vertical: 60),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-              colors: [
-                Colors.white,
-                Colors.white,
-              ],
-            ),
-            ),
-            child: Image.asset("assets/images/logo.png"),
-            height: 168,
-          ),
-          Center(
-            child: SingleChildScrollView(
-              child: Card(
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(50), topRight: Radius.circular(50)),),
-                color: Colors.black,
-                margin: const EdgeInsets.only(top:125),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+        body: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 60, bottom: 60),
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    width: 200,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.black87,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(50),
+                          topRight: Radius.circular(50)
+                      )
+                  ),
                   child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      SizedBox(height: 20),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white,
-                        ),
-                        child: TextField(
-                        decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(20),
-                            border: OutlineInputBorder(),
-                            hintText: "Username:",
-                            hintStyle: TextStyle(color: Colors.grey, fontSize: 16,)
-                        ),
-                      ),
-                      ),
-                      SizedBox(height: 12,),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white,
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(),
-                              hintText: "Password:",
-                              hintStyle: TextStyle(color: Colors.grey, fontSize: 16,)
-                          ),
-                          obscureText: true,
-                        ),
-                      ),
-                      SizedBox(height: 12,),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white,
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.all(20),
-                              border: OutlineInputBorder(),
-                              hintText: "Email:",
-                              hintStyle: TextStyle(color: Colors.grey, fontSize: 16,)
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40, bottom: 40),
+                        child: Text("Register",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold
                           ),
                         ),
                       ),
-                      SizedBox(height: 12,),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white,
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.all(20),
-                              border: OutlineInputBorder(),
-                              hintText: "Name:",
-                              hintStyle: TextStyle(color: Colors.grey, fontSize: 16,)
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 12,),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white,
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.all(20),
-                              border: OutlineInputBorder(),
-                              hintText: "LastName:",
-                              hintStyle: TextStyle(color: Colors.grey, fontSize: 16,)
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 25),
-                      RaisedButton(
-                        color: Colors.purple[200],
-                        textColor: Colors.white,
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        onPressed: () {
-                          _login(context);
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text("Register"),
-                            if (_loading)
-                              Container(
-                                height: 20,
-                                width: 20,
-                                margin: const EdgeInsets.only(left: 20),
-                                child: CircularProgressIndicator(),
-                              )
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                              '¿Ya te encuentras registrado?',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          FlatButton(
-                              textColor: Colors.purple[200],
-                              onPressed: (){
-                                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => LoginPage()));
-                              },
-                              child: Text("Log in")
-                          ),
-                        ],
+                      Form(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20, right: 20),
+                            child: Column(
+                              children: [
+                                TextField(
+                                  decoration: _inputDecoration(text: "Firstname", icon: Icons.text_fields),
+                                  keyboardType: TextInputType.name,
+                                  onChanged: (value) => setState(() => email = value),
+                                ),
+                                SizedBox(height: 10),
+                                TextField(
+                                  decoration: _inputDecoration(text: "Lastname", icon: Icons.text_fields),
+                                  keyboardType: TextInputType.name,
+                                  onChanged: (value) => setState(() => lastName = value),
+                                ),
+                                SizedBox(height: 10),
+                                TextField(
+                                  decoration: _inputDecoration(text: "E-mail", icon: Icons.email),
+                                  keyboardType: TextInputType.emailAddress,
+                                  onChanged: (value) => setState(() => email = value),
+                                ),
+                                SizedBox(height: 10),
+                                TextField(
+                                  decoration: _inputDecoration(text: "Password", icon: Icons.password),
+                                  keyboardType: TextInputType.visiblePassword,
+                                  obscureText: true,
+                                  onChanged: (value) => setState(() => password = value),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+                                  child: Row(
+                                    children: [
+                                      Expanded(child: Text("Register as organizer?",
+                                        style: TextStyle(color: Colors.white, fontSize: 16,),
+                                      )),
+                                      Switch(value: isOrganizer, onChanged: (value) {
+                                        setState(() {
+                                          this.isOrganizer = !this.isOrganizer;
+                                        });
+                                      })
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10, right: 10),
+                                  child: Row(
+                                    children: [
+                                      Expanded(child: Text("I accept the terms and conditions",
+                                        style: TextStyle(color: Colors.white, fontSize: 16,),
+                                      )),
+                                      Switch(value: accept, onChanged: (value) {
+                                        setState(() {
+                                          this.accept = !this.accept;
+                                        });
+                                      })
+                                    ],
+                                  ),
+
+                                ),
+                                SizedBox(height: 10),
+                                !processing ?
+                                ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      processing = true;
+                                      message = '';
+                                    });
+                                    // loginAction(email, password);
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                                    fixedSize: MaterialStateProperty.all<Size>(Size.fromWidth(500)),
+                                  ),
+                                  child: Text("Register"),
+                                ) : CircularProgressIndicator(),
+                                SizedBox(height: 20,),
+                                Text(message, style: TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 15
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("you are already registered?", style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15
+                                      ),
+                                    ),
+                                    MaterialButton(
+                                      textColor: Colors.purple[200],
+                                      onPressed: (){
+                                        Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => LoginPage()));
+                                      },
+                                      child: Text("Log in")
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(height: 30,),
+                              ],
+                            ),
+                          )
                       )
                     ],
                   ),
                 ),
-              ),
+              ],
             ),
           ),
-        ],
-      ),
+        )
     );
   }
 
-  void _login(BuildContext context) {
-    if(!_loading){
-      setState(() {
-        _loading = true;
-      });
-    }
-  }
+
 }
+
+InputDecoration _inputDecoration({required String text, required IconData icon}) => InputDecoration(
+    labelText: text,
+    border: UnderlineInputBorder(
+        borderSide: BorderSide(color: Colors.white)
+    ),
+    focusedBorder: UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.white),
+    ),
+    suffixIcon: Icon(icon),
+    fillColor: Colors.white,
+    filled: true
+);
